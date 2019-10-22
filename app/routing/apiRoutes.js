72fa2422b69aa2912b676b08c9f265d02d9beac0
SirHexxus <jamesmichaelstacy@gmail.com>
2019-10-22 13:06:00 -0700

    initial commenting to delineate file purpose, and created initial export statements

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,6 @@
+//  This file shall contain the Express routing information for all API GET/POST requests
+
+
+
+//  Exporting the Routes data. It will be required in server.js
+module.exports = apiRoutes;
