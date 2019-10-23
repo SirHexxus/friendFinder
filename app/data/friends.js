525bac884ac594e862ebd9a2f7b48c974d707541
SirHexxus <jamesmichaelstacy@gmail.com>
2019-10-23 12:58:00 -0700

    basic Friend constructor and export statement built

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+//  This file will hold the constructor for the Friend Object
+const Friend = (name, pic, scores) => {
+    this.name = name;
+    this.photo = pic;
+    this.scoreList = scores;
+}
+
+module.exports = Friend;
