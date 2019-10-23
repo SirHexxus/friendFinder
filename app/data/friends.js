70c65417addc42e1a797f955d8e588137cb4a554
SirHexxus <jamesmichaelstacy@gmail.com>
2019-10-23 13:06:00 -0700

    added methods to Friends constructor

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,23 @@
 //  This file will hold the constructor for the Friend Object
-const Friend = (name, pic, scores) => {
+const Friend = (name, pic) => {
     this.name = name;
     this.photo = pic;
-    this.scoreList = scores;
+    this.scores = [];
+    this.addScore = function(score) {
+        if(this.scores.length < 15) {
+            this.scores.push(score)
+        }
+        else {
+            return console.log("Too many scores!");
+        }
+    }
+    this.listScores = function() {
+        let temp;
+        for(let i = 0; i < this.scores.length; i++) {
+            temp += this.scores[i] + '\n';
+        }
+        return temp;
+    }
 }
 
 module.exports = Friend;
