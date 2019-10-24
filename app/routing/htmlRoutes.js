d846772d7eeca7ca3fa67be1a25dd7fb9a9dd08a
SirHexxus <jamesmichaelstacy@gmail.com>
2019-10-23 18:28:30 -0700

    added express and path info to html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,6 @@
 //  This file shall contain the Express routing information for all HTML requests
-
+const express = require('express');
+const path = require('path');
 
 
 //  Exporting the Routes data. It will be required in server.js
